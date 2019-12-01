20ffc60aed7ef4ea133fe529f751c1c01632c094
Bryan Szendel <bryan.szendel@gmail.com>
2019-11-30 17:08:36 -0700

    material UI installed and styled added to total revenue metric

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,40 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 // import Dashboard from './components/Dashboard';
 // import {getOrders} from './utils/endpoints.js';
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  Card,
+  CardContent,
+  CardActions,
+  Container
+} from '@material-ui/core';
 import axios from 'axios';
 const loader = require('./assets/spinning-circles.svg')
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    ...theme.typography.h4,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1),
+  },
+  container: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  card: {
+    maxWidth: 320,
+    minWidth: 275,
+    height: 150,
+    margin: 25
+  },
+}));
 
 const App = () => {
   const [orders, setOrders] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+
+  const classes = useStyles();
 
   const getOrders = () => {
     axios.get('http://localhost:6001/api/square/orders')
@@ -43,19 +71,37 @@ const App = () => {
   }, 0)
 
   function currencyFormatter(num) {
-    let baseNum = num.toString().slice(0, -2);
-    let decimals = num.toString().slice(-2);
-    return `$${baseNum}.${decimals}`
+    if (num.length <= 5) {
+      let baseNum = num.toString().slice(0, -2);
+      let decimals = num.toString().slice(-2);
+      return `$${baseNum}.${decimals}`
+    } else {
+      let baseThousands = num.toString().slice(-8, -5);
+      let baseHundreds = num.toString().slice(-5, -2);
+      let decimals = num.toString().slice(-2);
+      return `$${baseThousands},${baseHundreds}.${decimals}`
+    }
   }
 
   return (
     <div className="App">
-      <h1>{isLoading === true ? <img src={loader} alt='loading' /> : currencyFormatter(totalCollected)}</h1>
-      {/* {orders.map((order, index) => {
-        return (
-          <p key={index}>{order.id}</p>
-        )
-      })} */}
+      <Container className={classes.container}>
+        <Card raised className={classes.card}>
+          <CardContent>
+            <div
+              className={classes.root}
+            >
+              YTD Revenue
+            </div>
+            <div 
+              className={classes.root}>
+              {isLoading === true ? 
+              <img src={loader} alt='loading' /> : 
+              currencyFormatter(totalCollected)}
+            </div>
+          </CardContent>
+        </Card>
+      </Container>
     </div>
   );
 }
